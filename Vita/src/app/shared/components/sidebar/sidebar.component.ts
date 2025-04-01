f34316837d22eab45d120bc8fabfcc964736e51d
Walid Saifi <saifiwalid21@gmail.com>
2025-04-01 21:03:24 +0000

    ajouter sidbar admin prisen charge est dossier

--- Vita/src/app/shared/components/sidebar/sidebar.component.ts
+++ Vita/src/app/shared/components/sidebar/sidebar.component.ts
@@ -13,12 +13,27 @@ export class SidebarComponent implements OnInit {
   filteredMenuItems: any[] = [];
 
   allMenuItems = [
+
     {
       title: 'Tableau de bord ',
       icon: 'M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6',
       route: '/admin',
       roles: ['ADMINISTRATEUR']
     },
+
+    {
+      title: 'Validation Présence Chargé',
+      icon: 'M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z',
+      route: '/admin/validation-presences',
+      roles: ['ADMINISTRATEUR']
+    },
+    {
+      title: 'Validation Dossiers',
+      icon: 'M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z',
+      route: '/dossier', 
+      roles: ['ADMINISTRATEUR']
+    },
+   
     {
       title: 'Tableau de Bord Adhérent',
       icon: 'M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6',
